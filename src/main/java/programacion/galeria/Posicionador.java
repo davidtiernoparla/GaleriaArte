22aa9f58c20d8223f1116f89caa561bfce17ab69
davidtiernoparla <ortegahoyas.david@tiernoparla.es>
2023-02-03 17:12:59 +0100

    ADD Posicionador almacena posiciones constantes

---
+++ src/main/java/programacion/galeria/Posicionador.java
@@ -0,0 +1,13 @@
+package programacion.galeria;
+
+final public class Posicionador {
+    static final int POS_ELEM_ID = 0;
+    static final int POS_ELEM_NOM = 1;
+    static final int POS_ELEM_AUT = 2;
+    static final int POS_ELEM_TEC_MAT = 3;
+    static final int POS_ELEM_PRECIO = 4;
+    static final int POS_ELEM_ALT = 5;
+    static final int POS_ELEM_PESO = 6;
+    static final int POS_ELEM_PIEZ = 7;
+    static final int POS_ELEM_DESC = 8;
+}
